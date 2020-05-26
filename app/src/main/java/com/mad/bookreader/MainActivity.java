98bfeda903c6623acf009c25155b82ef28a51b7b
RyanLee <unconfigured@null.spigotmc.org>
2020-05-27 03:21:29 +0800

    added import files code

--- app/src/main/java/com/mad/bookreader/MainActivity.java
+++ app/src/main/java/com/mad/bookreader/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -72,6 +73,13 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 123 && resultCode == RESULT_OK) {
+            Uri selectedFile = data.getData();
         }
     }
 }
