83bc3bf4c5bc22b847ee7e985cf3b64db4741980
Ryan Lee <s10195732@connect.np.edu.sg>
2020-05-27 23:32:37 +0800

    immport can add book to recyclerview, but can't read yet

--- app/src/main/java/com/mad/bookreader/MainActivity.java
+++ app/src/main/java/com/mad/bookreader/MainActivity.java
@@ -3,24 +3,32 @@ package com.mad.bookreader;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.PathUtils;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.github.barteksc.pdfviewer.PDFView;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    final static String TAG="Main Activity";
+    final static String TAG = "Main Activity";
 
     List<importedBooks> listBooks;
 
@@ -29,15 +37,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         /*To add my own toolbar instead of the default*/
-        Toolbar toolbar=findViewById(R.id.app_bar);
+        Toolbar toolbar = findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
 
 
         //Create list and populate( populate To be removed)
-        listBooks=new ArrayList<>();
+        listBooks = new ArrayList<>();
         //for (int i=0;i<50;i++){
-        importedBooks b1=new importedBooks("Manga",R.drawable.isla,"testpdf.pdf");
-        importedBooks b2=new importedBooks("Kendo",R.drawable.isla2,"kendo.pdf");
+        importedBooks b1 = new importedBooks("Manga", R.drawable.isla, "testpdf.pdf");
+        importedBooks b2 = new importedBooks("Kendo", R.drawable.isla2, "kendo.pdf");
         listBooks.add(b1);
         listBooks.add(b2);
         //}
@@ -49,18 +57,18 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void recyclerFunction(List<importedBooks>bList){
-        RecyclerView rview=(RecyclerView)findViewById(R.id.bookCycler);
-        recyclerAdaptor rAdaptor=new recyclerAdaptor(bList);
-        GridLayoutManager gLayoutManager=new GridLayoutManager(this,3);
+    private void recyclerFunction(List<importedBooks> bList) {
+        RecyclerView rview = (RecyclerView) findViewById(R.id.bookCycler);
+        recyclerAdaptor rAdaptor = new recyclerAdaptor(bList);
+        GridLayoutManager gLayoutManager = new GridLayoutManager(this, 3);
         rview.setLayoutManager(gLayoutManager);
         rview.setAdapter(rAdaptor);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater=getMenuInflater();
-        inflater.inflate(R.menu.menu,menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -76,12 +84,22 @@ public class MainActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         //checks if requestCode and resultCode matches from above intent
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == 123 && resultCode == RESULT_OK) {
+        if (requestCode == 123 && resultCode == RESULT_OK) {
             Uri selectedFile = data.getData();
+            File file = new File(selectedFile.getPath());
+            final String[] split = file.getPath().split(":");
+            String filePath = split[1];
+            String fileName = filePath.substring(filePath.lastIndexOf("/")+1);
+            Log.v(TAG, fileName);
+            importedBooks book = new importedBooks(fileName, R.drawable.isla, fileName);
+            listBooks.add(book);
+            recyclerFunction(listBooks);
+
         }
     }
 }
